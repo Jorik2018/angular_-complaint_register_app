652e5d7010ee5c092683fb448cf081aa2abe76c9
Roger Vergara <roger.jvs.08@gmail.com>
2023-01-25 23:06:29 -0500

    Deploy v5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'angular_complaint_app',
+  appId: 'gob.regionancash.pe.denunciaform',
+  appName: 'Formulario de Denuncias GRA',
   webDir: 'www',
   bundledWebRuntime: false
 };
